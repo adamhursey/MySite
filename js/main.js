64f38ea3065fbabef3023be3c9b5bd8caaad9ce0
Adam Hursey <adamhursey24@gmail.com>
2019-06-11 02:44:53 -0400

    added projects and technical skills section

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var body = document.body,
+    overlay = document.querySelector('.overlay'),
+    cotdOverlay = document.querySelector('.overlay-module--cotd')
+    r5Overlay = document.querySelector('.overlay-module--r5');
+    cotdBtn = document.querySelector('.project-image--cotd');
+    r5Btn = document.querySelector('.project-image--r5');
+    closeOverlayBtns = document.querySelectorAll('.close-overlay');
+
+[].forEach.call(closeOverlayBtns, function(btn) {
+   btn.addEventListener('click', btn => {
+   overlay.setAttribute('aria-hidden', true);
+   body.classList.toggle('noscroll', false);
+   setTimeout(() => {r5Overlay.classList.toggle("u-hidden", false)}, 500);
+   setTimeout(() => {cotdOverlay.classList.toggle("u-hidden", false)}, 500);
+   overlay.scrollTop = 0;
+   })
+});
+
+cotdBtn.addEventListener('click', function() { 
+   overlay.setAttribute('aria-hidden', false);
+   r5Overlay.className += ' u-hidden';
+   body.classList.toggle('noscroll', true);
+   overlay.scrollTop = 0;
+   
+}, false);
+
+r5Btn.addEventListener('click', function() { 
+   overlay.setAttribute('aria-hidden', false);
+   cotdOverlay.className += ' u-hidden';
+   body.classList.toggle('noscroll', true);
+   overlay.scrollTop = 0;
+
+}, false);
+
+r5Btn.click();
